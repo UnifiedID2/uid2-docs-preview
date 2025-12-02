09352d9e87709373d913d94e2f33f375d9a6a6c5
Sunny Wu <sunny.wu@thetradedesk.com>
2025-12-02 16:39:14 +1100

    Update Prebid sidebar keys for guides

--- preview/sidebars.js
+++ preview/sidebars.js
@@ -120,7 +120,7 @@ const fullSidebar = [
                 {
                   type: 'category',
                   label: 'Prebid',
-                  key: 'Prebid-Web',
+                  key: 'Prebid-Web-Only-Guides',
                   link: {
                     type: 'doc',
                     id: 'guides/integration-prebid',
@@ -170,7 +170,7 @@ const fullSidebar = [
           {
             type: 'category',
             label: 'Prebid',
-            key: 'Prebid-Mobile',
+            key: 'Prebid-Web-And-Mobile-Guides',
             link: {
               type: 'doc',
               id: 'guides/integration-prebid',
